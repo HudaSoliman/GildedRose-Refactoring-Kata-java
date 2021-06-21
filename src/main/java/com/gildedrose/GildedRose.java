fe90c10ecb29809203e917a819291c32c58d49f2
hudasoliman <hudas@live.com>
2021-06-21 23:52:17 +0200

    Refactoring and handling the addition of the new conjured type

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -1,62 +1,100 @@
 package com.gildedrose;
 
 class GildedRose {
-    Item[] items;
+	Item[] items;
+	static final int QUALITY_MAX_VALUE = 50;
+	static final int BACKSTAGE_PASSES_SELLIN_LIMIT_1 = 10;
+	static final int BACKSTAGE_PASSES_SELLIN_LIMIT_2 = 5;
 
-    public GildedRose(Item[] items) {
-        this.items = items;
-    }
+	public GildedRose(Item[] items) {
+		this.items = items;
+	}
 
-    public void updateQuality() {
-        for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        items[i].quality = items[i].quality - 1;
-                    }
-                }
-            } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
+	public void updateQuality() {
+		for (Item item : items) {
+			updateItem(item);
+		}
+	}
 
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
+	private void updateItem(Item item) {
+		decrementSellIn(item);
 
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-                    }
-                }
-            }
+		ItemType itemType = ItemType.getItemType(item);
+		switch (itemType) {
+		case AGED_BRIE:
+			updateAgedBrie(item);
+			break;
+		case BACKSTAGE_PASSES:
+			updateBackstagePasses(item);
+			break;
+		case SULFURAS:
+			// do nothing
+			break;
+		case CONJURED:
+			updateConjured(item);
+			break;
+		default:
+			updateDefault(item);
+			break;
+		}
+	}
 
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                items[i].sellIn = items[i].sellIn - 1;
-            }
+	private void incrementQuality(Item item, int n) {
+		if (item.quality + n <= QUALITY_MAX_VALUE) {
+			item.quality += n;
+		} else {
+			item.quality = QUALITY_MAX_VALUE;
+		}
+	}
 
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                items[i].quality = items[i].quality - 1;
-                            }
-                        }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality;
-                    }
-                } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
-                    }
-                }
-            }
-        }
-    }
+	private void decrementQuality(Item item, int n) {
+		if (item.quality - n >= 0) {
+			item.quality -= n;
+		} else {
+			item.quality = 0;
+		}
+	}
+
+	private void updateAgedBrie(Item item) {
+		if (item.sellIn < 0) {
+			incrementQuality(item, 2);
+		} else {
+			incrementQuality(item, 1);
+		}
+	}
+
+	private void updateConjured(Item item) {
+		if (item.sellIn >= 0) {
+			decrementQuality(item, 2);
+		} else {
+			decrementQuality(item, 4);
+		}
+	}
+
+	private void updateBackstagePasses(Item item) {
+		if (item.sellIn < 0) {
+			item.quality = 0;
+		} else if (item.sellIn <= BACKSTAGE_PASSES_SELLIN_LIMIT_2) {
+			incrementQuality(item, 3);
+		} else if (item.sellIn <= BACKSTAGE_PASSES_SELLIN_LIMIT_1) {
+			incrementQuality(item, 2);
+		} else {
+			incrementQuality(item, 1);
+		}
+	}
+
+	private void updateDefault(Item item) {
+		if (item.sellIn < 0) {
+			decrementQuality(item, 2);
+		} else {
+			decrementQuality(item, 1);
+		}
+	}
+
+	private void decrementSellIn(Item item) {
+		if (!ItemType.getItemType(item).equals(ItemType.SULFURAS)) {
+			item.sellIn--;
+		}
+	}
+
 }
