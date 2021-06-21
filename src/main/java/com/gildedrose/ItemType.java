eebbd52ddd4facb935138f7e57d7a4a5b86c7cd0
hudasoliman <hudas@live.com>
2021-06-21 23:48:10 +0200

    Adding Enum for ItemTypes

---
+++ src/main/java/com/gildedrose/ItemType.java
@@ -0,0 +1,32 @@
+package com.gildedrose;
+
+import java.util.Optional;
+import java.util.stream.Stream;
+
+public enum ItemType {
+	AGED_BRIE("Aged Brie"),
+	BACKSTAGE_PASSES("Backstage passes"),
+	SULFURAS("Sulfuras"),
+	CONJURED("Conjured"),
+	DEFAULT("*");
+	
+	private String type;
+	
+	private ItemType(String type ) {
+		this.type = type;
+	}
+	
+	public static ItemType getItemType(Item item) {
+		
+		Optional<ItemType> type = 
+				Stream.of(ItemType.values())
+				.filter(i-> !i.equals(ItemType.DEFAULT) && isMatchingType(item.name, i.type))
+				.findFirst();
+		
+		return type.isPresent() ? type.get() : DEFAULT;
+	}
+	
+	private static  boolean isMatchingType(String name, String type) {
+		return name.toLowerCase().matches("^"+type.toLowerCase()+".*$");
+	}
+}
